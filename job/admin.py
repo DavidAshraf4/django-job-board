8396373284d7ea3fa3aab834d8163f372ef69cfb
DavidAshraf4 <davidashraf4421@gmail.com>
2022-01-23 21:29:16 +0200

    add job views & url

--- job/admin.py
+++ job/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Job
+from .models import Job , Category
 
 admin.site.register(Job) #add Jon on admin control
+admin.site.register(Category)
---
+++ job/urls.py
@@ -0,0 +1,8 @@
+from django import views
+from django.urls import include , path
+from . import views
+urlpatterns = [
+   path('',views.job_list,),
+   path('<int:id>',views.job_detail,)
+
+]
